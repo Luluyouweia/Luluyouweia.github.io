10ce58206c000ebc8ae9cb1ee5ef616e6e83ea75
lucel <102407505+Luluyouweia@users.noreply.github.com>
2022-11-20 17:33:10 +0800

    Add files via upload
    鹿客音乐

---
+++ music/README.md
@@ -0,0 +1,37 @@
+### **关于Lucel音乐**
+
+ _Lucel音乐开发者 Lulu_ 
+
+ _ :cn: 来自中国_ 
+
+[点击进入网站:Lucel Music 鹿客音乐](https://lucel.pages.dev/music/)
+
+### 关于
+1. 本网页不用于商业用途
+2. 本网页仅用于演示
+3. 网站由[Cloudflare](https://www.cloudflare.com)托管
+
+ **格言** 
+> 追求美好，碌碌有为。
+>————Lucel独立开发者 鹿鹿
+
+### 音乐资源来源
+
+|  网易云音乐 | 无  |
+|---|---|
+|  Lucel音乐 | 其他  |
+
+
+### 音乐数据查看方法
+
+```
+    <script src="https://luspecial.vercel.app/music/data.js"></script>
+    //data[x]为音乐文件
+    //id是网易云音乐id
+    //mid是任意链接
+    //source是iframe框架链接
+    console.log(data[0]);//=>...
+```
+
+
+ 
---
+++ music/data.js
@@ -0,0 +1,121 @@
+	var data=new Array();//Data数据数组
+		data[0]={
+			title: "After Tomorrow",
+			text: "网易游戏特辑音乐",
+			id: "1329466574",
+			img:"https://lucel.pages.dev/background.jpg",
+			type:"游戏;精选;高级"}
+		data[1]={
+			title: "花之舞",
+			text:"Flower Dance",
+			id:"552194857",
+			img: "https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"}
+		data[2]={
+			title: "亡灵序曲",
+			text:"Intro:The Dawn 李嘉诚(Jacong)",
+			id:"1467730592",
+			img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+		data[3]={
+			title: "优雅嘲讽(原曲Groove Battle)",
+			text:"Up主圆周率在地球上已留学多年",
+			id:"1957256466",
+			img: "https://t15.baidu.com/it/u=2558305100,3869169614&fm=220&app=103&size=f558,372&n=0&f=JPEG&fmt=auto&maxorilen2heic=2000000"}
+		data[4]={
+			title: "Tennessee",
+			text:"点击播放，源自纪录影片《珍珠港》",
+			id:"568888000",
+			img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+		data[5]={
+			title: "Beyond The Memory",
+			text:"点击播放",
+			id:"5307982",
+			img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+		data[6]={
+			title: "オラリオン",
+			text:"点击播放",
+			mid:"https://lucel.pages.dev/cloud/file/private/musicZjdcrts2-2.mp3",
+			img: ""}
+		data[7]={
+			title: "营地中心",
+			text:"网易游戏背景音乐",
+			id:"1329466573",
+			img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/ef19750eee8e8d4f50e841cc390ccd02fcab5bdf30495623655b4b7ecdcd945b45f6eaa770cb5717d4f956801e38963a?pictype=scale&from=30113&version=3.3.3.3&fname=R-C.jpg&size=750"}
+		data[8]={
+			title: "狮尾之舞",
+			text:"米哈游游戏背景音乐",
+			id:"1981803400",
+			img: "https://webstatic.mihoyo.com/upload/contentweb/2022/07/24/3c16216d6e7f1dc788ba783d3a793d9e_8358466801763713902.jpg"}
+		data[9]={
+			title: "钢铁洪流进行曲",
+			text:"国庆70周年庆典背景音乐之一,著名音乐",
+			id:"1394369908",
+			img: "https://gimg3.baidu.com/search/src=https%3A%2F%2Fbkimg.cdn.bcebos.com%2Fsmart%2F3ac79f3df8dcd100baa1876012c15010b912c8fc8112-bkimg-process%2Cv_1%2Crw_1%2Crh_1%2Cmaxl_300%2Cpad_1&refer=http%3A%2F%2Fwww.baidu.com&app=2021&size=w348&n=0&g=0n&er=404&q=75&fmt=auto&maxorilen2heic=2000000"}
+        data[10]={
+            title: "aLize",text: "一首燃曲，澤野弘之作之",
+            id: "1313052233",img: "https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+        data[11]={
+            title: "千本桜",text: "歌手：黒うさP / 初音ミク",
+            id: "26096272",img: "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"}
+        data[12]={
+            title: "Never Gonna Give You Up",text: "你被骗了，但是自愿。<br>维诺.理查德.汪苏泷",
+            id:"5221167",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/1104801e986ac4e54f0eb336cc7fa95e774ad70d1ff3dac8a3aefd119ec6aa9681fb519a368c93b9f1d2149679cdb76b?pictype=scale&from=30113&version=3.3.3.3&fname=NGGYU.jpg&size=750"}
+        data[13]={
+            title: "星天(Star Sky)",text:"常用战忽局BGM,燃起来了！",
+            id: "31654478",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+        data[14]={
+            title: "Astronomia",text:"黑人抬棺主题交响乐",
+            id: "430114655",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/d972e5420adff2d193af09a50066665b7df78241564afb10f3ae462b268b3bd2b547d8f191129de9e2b7b11dc9b35346?pictype=scale&from=30113&version=3.3.3.3&fname=HRTG.jpg&size=750"}
+        data[15]={
+            title:"",text:"平静的小曲，网友神评：“听着这音乐打代码，感觉我在拯救世界！！！”",
+            id:"2112355",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/90c5f8a7a662afcf71306e72e4e18e9f8e07c5fce6f52c4718e2967764866651c65d6717089bfc9b96defcaf3ad934c9?pictype=scale&from=30113&version=3.3.3.3&fname=DMKJ.jpg&size=750"}
+        data[16]={
+            title:"苏维埃进行曲",text:"不知道该怎么描述",
+            id:"5042951",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+        data[17]={
+            title:"So far away",text:"网易云音乐电音榜单",
+            id:"521416693",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"}
+        data[18]={
+            title:"<h4 style='color:red'>MV</h4>白天不懂夜的黑",text:"嗯...这图片你敢直接看吗？！“年轻人不要太气盛”“不气盛叫年轻人吗”“是龙得盘着，是虎得卧着”“刘华强，我告诉你，你今天要是敢踏出这房门一步...”“痛快，我就喜欢你这脾气”[doge]",
+            source:"https://player.bilibili.com/player.html?aid=91073180&bvid=BV11j411f7ak&cid=155517433&page=1",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/ae9b8017098b60fff4639c2cb7206dcc7ab2772269afa6892afe13c629fde1e4eb7d4dc08d046406769805408b7e9229?pictype=scale&from=30113&version=3.3.3.3&fname=v2-b869d0b7668b2cb7f73c90bf252dede6_r.jpg&size=750"}
+        data[19]={
+            title: "Lu Be-Free",text:"方特：熊出没奇幻空间",id:"449818326",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"
+        }
+        data[20]={
+            title:"耍把戏",text:"流行歌曲,阿禹ayy",id:"1942278883",img:"https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/7ee2d65d34641da4d41517e9979276e589a3e03b782df16e464a5e2bf0b8da412c142880b7a490f67beda834ef447751?pictype=scale&from=30113&version=3.3.3.3&fname=Screenshot_20220806_222718_cn.wps.moffice_eng_edit_96068856515548.jpg&size=750"
+        }
+        data[21]={
+            title:"疾如猛火",text:"燃起来了！",id:"1492283139",img:"https://uploadstatic.mihoyo.com/contentweb/20200319/2020031921552395638.jpg"
+        }
+     data[22]={
+            title:"寒光裂涛",text:"二创音乐",id:"1938344204",img:""
+        }
+        data[23]={
+            title:"Free Lucky",text:"纯音乐",id:"1392600538",img:""
+        }
+        data[24]={
+            title:"Time to pretend",text:"让我们再次伟大哈哈哈！！！",id:"1899726376",img:""
+        }
+        data[25]={
+            title:"带我去找夜生活",text:"告五人作曲",id:"1410647903",img:"https://images.unsplash.com/photo-1515981590392-502e83bbf224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1100&q=80"
+        }
+        data[26]={
+            title:"飞向遥远的天空",text:"遠い空へ【ヨスガノソラ メインテーマ】",id:"411755222",img:""
+        }
+        data[27]={
+            title:"尘世闲游",text:"悠然，悠然，不过，？？？",id:"1817410059",img:""
+        }
+        data[28]={
+            title:"环太平洋",text:"猛燃",id:"26625301",img:""
+        }
+        data[29]={
+            title:"<h4 style='color:red'>MV</h4>永远不会放弃您(Never Gonna Give You Up 豪华版)",text:"您要被骗了！",source:"https://player.bilibili.com/player.html?aid=601888108&bvid=BV1vB4y1574y&cid=802251467&page=1",img:"https://www.concierto.cl/wp-content/uploads/2021/02/Rick-Astley.jpg"
+        }
+        data[30]={
+            title:"空行不羁",text:"压迫感就此显现",id:"1879108774",img:""
+        }
+        data[31]={
+            title:"His theme",text:"音乐源于传说之下",id:"39227633",img:""
+        }
+        data[32]={
+            title:"只因你太美",text:"小黑子露出只因脚了吧",id:1340439829,img:""
+        }
