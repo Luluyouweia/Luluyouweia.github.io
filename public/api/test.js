5b24eba8c7000a08b839fbf17409fa0dc0323489
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2023-02-18 15:23:56 +0000

    git commit
    Signed-off-by: 逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>

---
+++ public/api/test.js
@@ -0,0 +1,34 @@
+/*DOC:
+-参数：
+   .必需：title,text,article
+   
+   .可选：date,method
+    
+-函数：
+   *** 警示：
+         任何api函数必须在数据最开始声明
+         例：date:"endl(xxx)西西物质魏骏杰"
+   ***
+*/ 
+let data={
+    lists:[
+        {
+            title: "代办i1",
+            text:"任务",
+            date:"endl(2023/2/18)结束时间:2023/2/28",
+            article:`
+              请及时完成今日作业！
+              [btn:查看作业(www.baidu.com)]
+            `,
+            method:"unable"
+        },
+        {
+            title: "代办2",
+            text:"作业",
+            date:"2023-3-3",
+            article:`
+                [img:tuchuang.voooe.cn/images/2023/01/24/65261833.jpg]
+            `
+        }
+    ]
+}
