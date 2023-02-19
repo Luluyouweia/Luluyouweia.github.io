c431262f11d646e9507bb7862c892fa5ab017f6b
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2023-02-19 06:36:32 +0000

    update public/api/family.js.
    Signed-off-by: 逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>

--- public/api/family.js
+++ public/api/family.js
@@ -13,21 +13,23 @@
 let data={
     lists:[
         {
-            title: "代办i1",
-            text:"任务",
-            date:"endl(2023/2/18)结束时间:2023/2/28",
+            title: "2月19日电学作业",
+            text:"@逯进学同志",
+            date:"endl(2023/2/19)结束时间:2023/2/19 23:59",
             article:`
               请及时完成今日作业！
-              [btn:查看作业(www.baidu.com)]
+              [img:https://tuchuang.voooe.cn/images/2023/02/19/mmexport1676642196848_edit_348354764544761.jpg]
+              [btn:查看作业(https://www.wjx.top/vm/r99RUG2.aspx#)]
             `,
             method:"unable"
         },
         {
-            title: "代办2",
-            text:"作业",
-            date:"2023-3-3",
+            title: "待办2",
+            text:"无",
+            date:"2023/2/19",
             article:`
-                [img:tuchuang.voooe.cn/images/2023/01/24/65261833.jpg]
+                关于家庭好地方<br/>
+                已完成阅读
             `
         }
     ]
