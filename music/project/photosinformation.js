ce50b627fa583c3d447a4dd36a89935378956a13
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2023-06-04 12:09:15 +0000

    add music/project/photosinformation.js.
    Signed-off-by: 逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>

---
+++ music/project/photosinformation.js
@@ -0,0 +1,77 @@
+		var img=new Array();
+			img[0]="pictures/main.jpg";//本地
+			img[1]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1651450661048-be61eba55f1a?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=joshua-lawrence-_GPn7zmkjD4-unsplash.jpg";//链接 链接至  Unplash 图库			img[1]
+            img[2]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1621290102989-e9865bef2dcf?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=vlad-d-nqSeto6vWfU-unsplash.jpg";
+			img[3]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1649605184337-c4dc52c6e32e?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=parsa-mahmoudi-nfi-TXXzwjI-unsplash.jpg&w=1920";
+			img[4]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1538428494232-9c0d8a3ab403?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=edward-he-uKyzXEc2k_s-unsplash.jpg&w=2400";
+			img[5]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1652274801662-25e4ee30534b?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=rogov-ca-XH8HTkT9bX4-unsplash.jpg";
+			img[6]="https://images.unsplash.com/photo-1654190556461-3919acf03f0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
+            img[7]="https://images.unsplash.com/photo-1623333769926-a34d46b5fbdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80";
+            img[8]="https://images.unsplash.com/photo-1652900439414-c0e834dcfa2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80";
+            img[9]="https://images.unsplash.com/photo-1554485686-5bf9ebe68393?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80";
+            img[10]="https://images.unsplash.com/photo-1516545595035-b494dd0161e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+            img[11]="https://images.unsplash.com/photo-1569660003459-6c30601dc19c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+            img[12]="https://images.unsplash.com/photo-1542931287-023b922fa89b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80";
+
+		var amusic=new Array(); 
+			amusic[0]="";
+			amusic[1]="https://music.163.com/outchain/player?type=2&id=5276811&auto=1&height=32";
+			amusic[2]="https://music.163.com/outchain/player?type=2&id=1394369908&auto=1&height=32";
+			amusic[3]="https://music.163.com/outchain/player?type=2&id=5221167&auto=1&height=32";
+			
+			//aLIEz
+			amusic[5]="https://music.163.com/outchain/player?type=2&id=29307041&auto=1&height=32";
+			
+			//rise
+			amusic[6]="https://music.163.com/outchain/player?type=2&id=28283137&auto=1&height=32";
+			
+			//英雄的黎明
+			amusic[7]="https://music.163.com/outchain/player?type=2&id=393705&auto=1&height=32";
+			
+			//sold out
+			amusic[8]="https://music.163.com/outchain/player?type=2&id=31010566&auto=1&height=32";
+			
+			//千本樱
+			amusic[9]="https://music.163.com/outchain/player?type=2&id=1339421617&auto=1&height=32";
+			
+			//Minecraft
+			amusic[10]="https://music.163.com/outchain/player?type=2&id=4010201&auto=1&height=32";
+			
+			//明日 营地中心
+			amusic[11]="https://music.163.com/outchain/player?type=2&id=1329466573&auto=1&height=32";
+					
+			//After tomorrow
+			amusic[12]="https://music.163.com/outchain/player?type=2&id=1329466574&auto=1&height=32";
+			
+			//在野外
+			amusic[13]="https://music.163.com/outchain/player?type=2&id=1329466553&auto=1&height=32";
+			
+			//last reunion
+			amusic[14]="https://music.163.com/outchain/player?type=2&id=32102855&auto=1&height=32";
+			
+			//LOVE Theme from TIGA 
+			amusic[15]="https://music.163.com/outchain/player?type=2&id=4968365&auto=1&height=32";
+
+            //起风了
+            amusic[16]="https://music.163.com/outchain/player?type=2&id=1330346841&auto=1&height=32"
+    
+            //营地外
+            amusic[17]="https://music.163.com/outchain/player?type=2&id=1329466553&auto=1&height=32";
+
+            //Soviet March
+            amusic[18]="https://music.163.com/outchain/player?type=2&id=5042951&auto=1&height=32";
+    
+            //Just Blue
+            amusic[19]="https://music.163.com/outchain/player?type=2&id=19067286&auto=1&height=32";
+
+            //螺丝刀，螺丝刀，半夜起来安第板
+            amusic[20]="https://music.163.com/outchain/player?type=2&id=4023379&auto=1&height=32";
+
+            //Song from a Secret Garden
+            amusic[21]="https://music.163.com/outchain/player?type=2&id=4278298&auto=1&height=32";
+
+            //瓦妮莎的微笑——每段句子读2遍
+            amusic[22]="https://music.163.com/outchain/player?type=2&id=5038193&auto=1&height=32";
+
+            //多冷的隆冬
+            amusic[23]="https://music.163.com/outchain/player?type=2&id=1370913370&auto=1&height=32";
