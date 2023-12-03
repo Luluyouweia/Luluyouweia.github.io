914a07043adf381ac284ca9392153932ea4903ac
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2023-12-03 10:21:44 +0000

    update public/api/app.js.
    Signed-off-by: 逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>

--- public/api/app.js
+++ public/api/app.js
@@ -1,3 +1,5 @@
+eval((function(e,f,a,d,c,g){c=function(b){return((b<f?"":c(parseInt(b/f)))+(35<(b%=f)?String.fromCharCode(b+29):b.toString(36)))};if(!"".replace(/^/,String)){for(;a--;)g[c(a)]=d[a]||c(a);d=[function(b){return g[b]}];c=function(){return"\\w+"};a=1}for(;a--;)d[a]&&(e=e.replace(new RegExp("\\b"+c(a)+"\\b","g"),d[a]));return e})('g h(a,b){5 6=`<1 i="j"2="7:0;k: 0;l: 0;8: 0;m: 9;n:o%;p-q: r;s-t: u;"><c 2="v: w(0,0,0,0.y);z: A;8: B;7: C;"D="3.d.E(F.e.e)">x</c><f G="\u5bf9\u8bdd\u6846"2="H-I: J%">\u63d0\u793a</f><1 2="K: 9;">${a}</1></1>`;5 4=3.L("1");4.M=6;3.d.N(4);O P}',52,52," section style document Element let Inner top right auto   span body parentNode div function msgbox class ModelBox bottom left margin width 75 max height 60vh border radius 12px color rgb  28 position absolute 10px 7px onclick removeChild this title font size 95 overflow createElement innerHTML appendChild return true".split(" "),0,{}));
+
 if(application){
     if(application.app=="鹿客英语"){
         if(application.version<1){
@@ -6,7 +8,7 @@ if(application){
     }
     if(application.app=="鹿客"){
         if(application.version<114){
-            alert("检测到更新！");
+            msgbox("好好好","什么年代了你还不更新是吧")
         }
     }
 }
